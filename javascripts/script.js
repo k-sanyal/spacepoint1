b5cf46c804abaa99ce3900e4d86e80ee440c275c
k-sanyal <72350751+k-sanyal@users.noreply.github.com>
2022-06-30 11:24:29 +0300

    Add files via upload

---
+++ javascripts/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $(".departure").click( 
+function () { 
+$(".departure").toggleClass("departure2");
+  });
+  $(".destination").click( 
+function () { 
+$(".destination").toggleClass("destination2");
+  });
+
+  $(".trackNum").click( 
+function () { 
+$(".trackNum").toggleClass("trackNum2");
+  });
+
+});
+
